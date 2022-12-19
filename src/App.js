9ccbaffd8c6f39c5b391a9630196cc6224660832
sefadalklc <sefadalk11@gmail.com>
2022-12-19 21:49:36 +0300

    search algorithms is created

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+import {Users} from "./user"
+
+function App() {
+  const [query,setQuery] = useState("");
+
+ 
+  return (
+    <div className="App">
+     <input 
+     type="text"
+      placeholder='Search...'
+       className="search" 
+       onChange={(e) => setQuery(e.target.value)} 
+       /> 
+     <ul className="list">
+      {Users.filter ((user) =>
+       user.first_name.toLowerCase().includes(query)
+      
+      ).map((user) => (
+        <li key={user.id} className="listItem">{user.first_name}</li>
+      ))}
+      
+     </ul>
+    </div>
+  );
+}
+
+export default App;
